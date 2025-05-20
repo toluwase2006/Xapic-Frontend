fdd6bd84ccc223c54078da702b69f8637a8d2a4d
toluwase2006 <08067600639te@gmail.com>
2025-05-20 22:32:39 +0100

    Done with the LandPage Desktop

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,7 +5,7 @@ import { RiArrowRightUpLine } from "react-icons/ri";
 import heroVector from "../assets/images/Vector 165.png"
 import secondSectionleftImage from "../assets/images/Group 17.png"
 import { FaArrowRight } from "react-icons/fa6";
-import imgGif1 from "../assets/gif/xap gif 1.gif"
+import secondSectionRightImage from "../assets/images/Vision.png"
 import recruitmentIcon from "../assets/images/fluent-mdl2_recruitment-management.png"
 import softwareDevelopmentIcon from "../assets/images/carbon_development.png"
 import cloudSolutionIcon from "../assets/images/eos-icons_cloud-computing-outlined.png"
@@ -16,6 +16,7 @@ import circuitLinesBackground from "../assets/images/circuit-lines@2xl.ee1ad3dd.
 import companiesLogo1 from "../assets/Logos/Frame 313.png"
 import companiesLogo2 from "../assets/Logos/Frame 314.png"
 import clientImage from "../assets/images/Group 14 (2).png"
+import footerXapicLogo from "../assets/images/Xapic Logo - Full Colour with White  1.png"
 
 const LandingPage = () => {
   return (
@@ -90,11 +91,7 @@ const LandingPage = () => {
 				</div>
 				<div className="w-[34.8125rem]">
 					<div className="relative w-[34.8125rem] h-[44.0625rem]">
-						<img src={imgGif1} alt="" className="w-full h-full" />
-						<div className="absolute left-10 bottom-10">
-							<p className="text-[#F2F2F2CC] text-3xl">Our Vision</p>
-							<p className="text-[1.25rem] font-light text-white max-w-[25rem] mt-1.5">Our vision is to be the leading catalyst for technological advancement and personal growth across Africa.</p>
-						</div>
+						<img src={secondSectionRightImage} alt="" className="w-full h-full" />
 					</div>
 					<div className="mt-[4.3125rem]">
 						<p className="text-[#3E4C57] text-[2.5rem]">The Mission we are on</p>
@@ -216,7 +213,7 @@ const LandingPage = () => {
 					<p className="mt-10 text-[2.5rem] text-[#323D46] font-bold">Don’t Just Take Our Words For It, Hear Our Client’s Review</p>
 				</div>
 				<div className="mt-16 flex gap-[1.5625rem]">
-					<div className="flex flex-col gap-[1.5625rem]">
+					<div className="grid grid-cols-2 gap-[1.5625rem]">
 						<div className="flex gap-2 items-center w-[40rem] h-[9.625rem] bg-white">
 							<img src={clientImage} alt="" className="w-[9.5625rem] h-full"/>
 							<div className="">
@@ -225,32 +222,6 @@ const LandingPage = () => {
 								<p className="text-[#3E4C57] w-[26.75rem]">I commend Xapictech for their dedication to excellence and for partnering with us to create opp...</p>
 							</div>
 						</div>
-						<div className="flex gap-2 items-center w-[40rem] h-[9.625rem] bg-white">
-							<img src={clientImage} alt="" className="w-[9.5625rem] h-full"/>
-							<div className="">
-								<p className="text-[#3E4C57] text-[1rem] mb-2 font-bold">DR. Summy Smart Francis</p>
-								<p className="text-[0.875rem] mb-4">SA to the Ondo State Governor on Entrepreneurship and youth Development</p>
-								<p className="text-[#3E4C57] w-[26.75rem]">I commend Xapictech for their dedication to excellence and for partnering with us to create opp...</p>
-							</div>
-						</div>
-						<div className="flex gap-2 items-center w-[40rem] h-[9.625rem] bg-white">
-							<img src={clientImage} alt="" className="w-[9.5625rem] h-full"/>
-							<div className="">
-								<p className="text-[#3E4C57] text-[1rem] mb-2 font-bold">DR. Summy Smart Francis</p>
-								<p className="text-[0.875rem] mb-4">SA to the Ondo State Governor on Entrepreneurship and youth Development</p>
-								<p className="text-[#3E4C57] w-[26.75rem]">I commend Xapictech for their dedication to excellence and for partnering with us to create opp...</p>
-							</div>
-						</div>
-						<div className="flex gap-2 items-center w-[40rem] h-[9.625rem] bg-white">
-							<img src={clientImage} alt="" className="w-[9.5625rem] h-full"/>
-							<div className="">
-								<p className="text-[#3E4C57] text-[1rem] mb-2 font-bold">DR. Summy Smart Francis</p>
-								<p className="text-[0.875rem] mb-4">SA to the Ondo State Governor on Entrepreneurship and youth Development</p>
-								<p className="text-[#3E4C57] w-[26.75rem]">I commend Xapictech for their dedication to excellence and for partnering with us to create opp...</p>
-							</div>
-						</div>
-					</div>
-					<div className="flex flex-col gap-[1.5625rem]">
 						<div className="flex gap-2 items-center w-[40rem] h-[9.625rem] bg-white">
 							<img src={clientImage} alt="" className="w-[9.5625rem] h-full"/>
 							<div className="">
@@ -293,6 +264,73 @@ const LandingPage = () => {
 		</div>
 
 		{/* Footer Section */}
+
+		<div className="bg-[#00011B] relative mt-[8.4375rem] h-[41rem]">
+			<div className="flex justify-center items-center">
+				<div className="flex justify-center items-center gap-[3rem] bg-[#000250] w-[90.8125rem] h-[10.875rem] absolute -top-20">
+					<p className="text-[1.75rem] text-white font-600">We are excited to be part of your educational journey!</p>
+					<div className="flex gap-6">
+						<button className="button bg-[#0096CC] w-[12.4375rem] text-[1.25rem]">
+							<p>APPLY HERE</p>
+							<FaArrowRight />
+						</button>
+						<button className="w-[15.5625rem] text-[1.25rem] text-white cursor-pointer border-white border-[1px]">
+							BOOK A CONSULTATION
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className="container pt-[12.3125rem]">
+				<div className="flex justify-between ">
+					<img src={footerXapicLogo} alt="" className="w-[19rem] h-[6.1875rem]" />
+					<div className="flex justify-between text-white text-1rem font-500 w-[57.53125rem]">
+						<div className="w-[11.6875rem]">
+							<p className="text-[#CCCDE7] text-[1.25rem] font-semibold mb-6">Schools</p>
+							<div className="flex flex-col gap-3">
+								<p>Design and Product</p>
+								<p>Engineering and Development</p>
+								<p>Summer Tech School</p>
+								<p>NYSC Training Programme</p>
+							</div>
+						</div>
+						<div>
+							<p className="text-[#CCCDE7] text-[1.25rem] font-semibold mb-6">Quicklinks</p>
+							<div className="flex flex-col gap-3 cursor-pointer">
+								<p className="navLinks"><a href="">About Us</a></p>
+								<p className="navLinks"><a href="">Our Services</a></p>
+								<p className="navLinks"><a href="">Career</a></p>
+								<p  className="navLinks"><a href="">Contact Us</a></p>
+								<p className="navLinks"><a href="">Design and Products</a></p>
+							</div>
+						</div>
+						<div>
+							<p className="text-[#CCCDE7] text-[1.25rem] font-semibold mb-6">Top Courses</p>
+							<div className="flex flex-col gap-3">
+								<p>Devops Engineering</p>
+								<p>Product Management</p>
+								<p>Front-end Engineering</p>
+								<p>Product Design</p>
+								<p>Graphics Design</p>
+								<p>Backend Engineering</p>
+							</div>
+						</div>
+						<div className="w-[14.75rem]">
+							<p className="text-[#CCCDE7] text-[1.25rem] font-semibold mb-6">Top Courses</p>
+							<div className="flex flex-col gap-3">
+								<p>Lagos : 2nd floor, Leasing House, C & I Leasing Drive, Off Bisola Durosinmi Etti Drive, Off Admiralty way, Lekki Phase 1, Lagos</p>
+								<p>Akure: Xapic Technologies Building, Opposite State High Court, Arakale Road, Akure</p>
+								<p>08167001859</p>
+								<p>info@xapictech.com</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<p className="text-center">Copyright © 2024. All right reserved.</p>
+
+
+			</div>
+
+		</div>
 
 	</>
   );
