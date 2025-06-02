6bdddf22876eb138a08cc0e0f1f6cb4fe9645a3e
toluwase2006 <08067600639te@gmail.com>
2025-06-02 16:52:06 +0100

    correc ted a nav space

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -54,7 +54,7 @@ const Navbar = () => {
                         <a href="/"><img src={xapicLogo} alt="" className='w-[7rem] h-10' /></a>
                         <CancelIcon className="fa-solid fa-xmark w-6 h-6 cursor-pointer" onClick={()=>setNavOpen(false)} />
                     </div>
-                    <nav className='flex flex-col gap-10 '>
+                    <nav className='flex flex-col gap-[1.875rem] '>
                         <ul className='flex flex-col gap-10 text-[1.25rem] text-[#3E4C57]'>
                             <li onClick={()=>setNavOpen(false)} className='navLinks'><Link to="/schools">Schools</Link></li>
                             <li onClick={()=>setNavOpen(false)} className='navLinks'><Link to="/services">Services</Link></li>
