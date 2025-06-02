4302e798f16bdfa820b2dfe4274acfd363674277
toluwase2006 <08067600639te@gmail.com>
2025-06-02 16:48:29 +0100

    corrected a space

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,7 +49,7 @@ const Navbar = () => {
                 </nav>
             </div>
             {navOpen && (
-                <div className={`flex flex-col gap-[2.6875rem] p-10  h-screen bg-white fixed top-0 left-0 transform transition-transform duration-600 ease-in-out w-full ${navOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                <div className={`flex flex-col gap-[2.6875rem] p-6  h-screen bg-white fixed top-0 left-0 transform transition-transform duration-600 ease-in-out w-full ${navOpen ? 'translate-x-0' : '-translate-x-full'}`}>
                     <div className='flex justify-between items-center'>
                         <a href="/"><img src={xapicLogo} alt="" className='w-[7rem] h-10' /></a>
                         <CancelIcon className="fa-solid fa-xmark w-6 h-6 cursor-pointer" onClick={()=>setNavOpen(false)} />
