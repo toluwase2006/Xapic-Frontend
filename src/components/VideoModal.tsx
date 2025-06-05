ec15dfd57e27d4c72fe54c18529f4927965e53fe
toluwase2006 <08067600639te@gmail.com>
2025-06-05 09:36:36 +0100

    added an animation to the video modal

--- src/components/VideoModal.tsx
+++ src/components/VideoModal.tsx
@@ -1,6 +1,8 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ModalContext} from "../useContext/ModalContext";
 import { FaTimes } from "react-icons/fa";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const VideoModal = () => {
 
@@ -8,11 +10,16 @@ const VideoModal = () => {
 
 
   if (videoValues.videoType === "close") return null
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+    });
+  }, []);
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="fixed h-full w-full inset-0 bg-[#e6e7fe]" onClick={() => controlVideoModal({url: "", videoType: "close"})}></div>
-      <div className="bg-black relative rounded-xl container h-[40%] sm:h-[70%] z-10">
+    <div className="fixed inset-0 flex items-center justify-center z-50" >
+      <div className="fixed h-full w-full inset-0 bg-transparent" onClick={() => controlVideoModal({url: "", videoType: "close"})}></div>
+      <div className="bg-black relative rounded-xl container h-[40%] sm:h-[70%] z-10" data-aos="fade-down" data-aos-delay="100" data-aos-duration="600">
         <div className="w-full absolute top-0 left-0 flex justify-end p-4  cursor-pointer" onClick={() => controlVideoModal({url: "", videoType: "close"})}>
           <FaTimes className="absolute right-8 w-8 h-8 text-white" />
         </div>
